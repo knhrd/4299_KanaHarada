eb6b151d02da90976de17bf4a38309eaf52883a6
knhrd <kana.harada@force-corp.co.jp>
2021-05-07 18:35:22 +0900

    css完成(仮)

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const btn = document.querySelector('.btn-menu');
+const nav = document.querySelector('.main-nav');
+btn.addEventListener('click', () => {
+    nav.classList.toggle('open-menu');
+    
+if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'close';
+} else{
+    btn.innerHTML = 'Menu';
+}
+});
